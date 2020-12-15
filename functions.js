e5f4b8b26a60f24602bdc471fea9dd0a3e01afc1
Nick Baker <ndbaker1@outlook.com>
2020-12-14 22:23:43 -0600

    base example code with templated new functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+exports.validateURL = (URL) => {
+  console.log('validate function called')
+  return 50;
+}
+
+exports.createCitations = (urllist) => {
+  console.log('citation function called');
+  return ['pepe'];
+}
