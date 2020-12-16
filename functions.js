5502df0c6e35a367baa82e8103c0ef99922d87a6
Nick Baker <ndbaker1@outlook.com>
2020-12-15 23:28:55 -0600

    removed the unused calls and turned the citations into an adaptive JSON card

--- functions.js
+++ functions.js
@@ -3,7 +3,39 @@ exports.validateURL = (URL) => {
   return 50;
 }
 
-exports.createCitations = (urllist) => {
-  console.log('citation function called');
-  return ['pepe'];
+exports.createCitation = (url) => {
+  return 'this new citation';
+}
+
+exports.printCitations = (bot, citations) => {
+  const cardJSON = generateCardTemplate();
+  for (const entry of citations) {
+    cardJSON.body[0].columns[0].items.push({
+      type: 'TextBlock',
+      text: entry.citation,
+      size: 'small', // maybe use 'medium'
+      wrap: true,
+    });
+  }
+  bot.sendCard(cardJSON, 'JSON Card could not be loaded...');
+}
+
+// returns the template for a JSON adaptive card
+function generateCardTemplate() {
+  return {
+    $schema: "http://adaptivecards.io/schemas/adaptive-card.json",
+    type: 'AdaptiveCard',
+    version: '1.0',
+    body: [
+      {
+        type: 'ColumnSet',
+        columns: [
+          {
+            type: 'Column',
+            items: []
+          }
+        ]
+      }
+    ]
+  };
 }
