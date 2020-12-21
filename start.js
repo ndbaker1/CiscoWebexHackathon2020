23a022f9da8f7138f0b07b6d5ef4c3531a7b7e2e
Nick Baker <ndbaker1@outlook.com>
2020-12-21 16:30:07 -0600

    find port in startup script as well. remove test references

--- start.js
+++ start.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 
 const configFile = 'config.json';
 fetch('http://localhost:4040/api/tunnels').then(res => res.json())
-.then((out) => {
-  console.log("updating config with latest webhook URL...");
-  const jsonConfig = JSON.parse(fs.readFileSync(configFile).toString());
-  jsonConfig.webhookUrl = out.tunnels[0].public_url;
-  fs.writeFileSync(configFile, JSON.stringify(jsonConfig, null, 2));
-  console.log('ngrok host: ', out.tunnels[0].public_url);
-})
+  .then((out) => {
+    console.log("updating config with latest webhook URL...")
+    const jsonConfig = JSON.parse(fs.readFileSync(configFile).toString());
+    const forwardedAddress = out.tunnels[0].config.addr
+    jsonConfig.port = forwardedAddress.substring(forwardedAddress.lastIndexOf(':') + 1)
+    jsonConfig.webhookUrl = out.tunnels[0].public_url;
+    fs.writeFileSync(configFile, JSON.stringify(jsonConfig, null, 2));
+    console.log('ngrok host: ', jsonConfig.webhookUrl, 'on port', jsonConfig.port);
+  })
